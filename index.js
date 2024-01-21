1280cb3d603b642ff9bd685981a89d9530e0a0a5
Mehedi Hasan <mehedihasan67705251@gmail.com>
2024-01-22 00:15:09 +0600

    adding manage booking api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config()
 const cors = require('cors');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const port =  process.env.PORT || 5000
 
 app.use(cors())
@@ -32,6 +34,7 @@ const menuCollection = client.db("BistroDB").collection("menu");
 const reviewCollection = client.db("BistroDB").collection("reviews");
 const cartCollection = client.db("BistroDB").collection("carts");
 const userCollection = client.db("BistroDB").collection("users");
+const paymentCollection = client.db("BistroDB").collection("payments");
 
 
 
@@ -66,6 +69,81 @@ if (!isAdmin) {
 }
 next();
 }
+
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { price } = req.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: parseInt(price*100),
+    currency: "usd",
+    payment_method_types:['card'],
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+app.get('/payments/:email', verifyToken, async (req, res) => {
+  const query = { email: req.params.email }
+  if (req.params.email !== req.decoded.email) {
+    return res.status(403).send({ message: 'forbidden access' });
+  }
+  const result = await paymentCollection.find(query).toArray();
+  res.send(result);
+})
+app.get('/payments', verifyToken, async (req, res) => {
+  
+  const result = await paymentCollection.find().toArray();
+  res.send(result);
+})
+// --------------
+app.patch('/payments/status/update/:id', verifyToken, async (req, res) => {
+  const id=req.params.id;
+  const filter = {_id: new ObjectId(id)}
+  const updatedDoc = {
+    $set:{
+      status:'Done'
+    }
+  }
+  const result =  await paymentCollection.updateOne(filter,updatedDoc);
+  res.send(result);
+})
+
+app.post('/payments', async (req, res) => {
+  const payment = req.body;
+  const paymentResult = await paymentCollection.insertOne(payment);
+
+  //  carefully delete each item from the cart
+  console.log('payment info', payment);
+  const query = {
+    _id: {
+      $in: payment.cartIds.map(id => new ObjectId(id))
+    }
+  };
+  const deleteResult = await cartCollection.deleteMany(query);
+
+  // send user email about payment confirmation
+  // mg.messages
+  //   .create(process.env.MAIL_SENDING_DOMAIN, {
+  //     from: "Mailgun Sandbox <postmaster@sandboxbdfffae822db40f6b0ccc96ae1cb28f3.mailgun.org>",
+  //     to: ["mhpiter8@gmail.com"],
+  //     subject: "Bistro Boss Order Confirmation",
+  //     text: "Testing some Mailgun awesomness!",
+  //     html: `
+  //       <div>
+  //         <h2>Thank you for your order</h2>
+  //         <h4>Your Transaction Id: <strong>${payment.transactionId}</strong></h4>
+  //         <p>We would like to get your feedback about the food</p>
+  //       </div>
+  //     `
+  //   })
+  //   .then(msg => console.log(msg)) // logs response data
+  //   .catch(err => console.log(err)); // logs any error`;
+
+  res.send({ paymentResult, deleteResult });
+})
+
+
+
 
 
 app.post('/users',async (req, res) => {
@@ -137,10 +215,47 @@ app.delete('/carts/:id',async (req,res)=>{
 })
 
 
-
+app.post('/menu',verifyToken,verifyAdmin,async (req,res)=>{
+  const item = req.body;
+  const result =await menuCollection.insertOne(item);
+  res.send(result)
+})
 app.get('/menu',async (req,res) => {
     const result =  await menuCollection.find().toArray();
     res.send(result)
+})
+app.get('/menu/:id',async(req,res)=>{
+  const id = req.params.id;
+// const query = {_id: new ObjectId(id)}
+const query = {_id:id}
+const result = await menuCollection.findOne(query);
+// console.log(result)
+res.send(result)
+})
+app.delete('/menu/:id',verifyToken,verifyAdmin,async (req,res)=>{
+  const id= req.params.id;
+  // const query ={_id: new ObjectId(id)}
+  const query = {_id:id}
+  const result =  await menuCollection.deleteOne(query);
+  res.send(result)
+})
+//
+app.patch('/menu/:id',verifyToken,verifyAdmin,async (req,res)=>{
+  const id= req.params.id;
+  const item = req.body;
+  // const filter ={_id: new ObjectId(id)}
+  const filter = {_id:id}
+  const updatedDoc = {
+    $set:{
+      name:item.name,
+      category:item.category,
+      price:item.price,
+      recipe:item.recipe,
+      image:item.image,
+    }
+  }
+  const result =  await menuCollection.updateOne(filter,updatedDoc);
+  res.send(result)
 })
 
 
